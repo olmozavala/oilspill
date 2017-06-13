4f431bd6b3367085d38129f04a6931c352c0d518
Esli_Jurado <moca92@gmail.com>
2017-06-13 10:54:45 -0500

    Added compatibility with two models and visualization types

--- OilSpillModel.m
+++ OilSpillModel.m
@@ -11,14 +11,31 @@ function OilSpillModel(modelConfig)
 % VDB                = Cantidad de petroleo dispersado en la sub-superficie
 [FechasDerrame,SurfaceOil,VBU,VE,VNW,VDB] = cantidades_por_dia;
 spillData          = OilSpillData(FechasDerrame,SurfaceOil,VBU,VE,VNW,VDB);
-VF                 = VectorFields();
+%Check model type
+if strcmp(modelConfig.model,'hycom')
+    VF                 = VectorFields();
+elseif strcmp(modelConfig.model,'adcirc')
+    %===============ADCIRC EXCLUSIVE==============
+    atmFilePrefix  = 'fort.74.'; % File prefix for the atmospheric netcdf files
+    oceanFilePrefix  = 'fort.64.'; % File prefix for the ocean netcdf files
+    uvar = 'u-vel';
+    vvar = 'v-vel';
+    global VF
+    VF                 = VectorFieldsADCIRC(0, atmFilePrefix, oceanFilePrefix, uvar, vvar);
+    VF                 = VF.readLists();
+    VF = VF.readUV(0, 115, modelConfig);
+end
 advectingParticles = false;          % Indicate when should we start reading the UV fields
 Particles          = Particle.empty; % Start the array of particles empty
 
 %  Initialize the figure for visualization of the particles
 if modelConfig.visualize
-  currentVis = VisualizeParticles;
-  currentVis.drawGulf();
+    currentVis = VisualizeParticles;
+    if strcmp(modelConfig.vistype,'coastline')
+        currentVis.drawGulf2d();
+    else
+        currentVis.drawGulf();
+    end
 end
 
 FinalParticles = {};
@@ -33,30 +50,39 @@ for currDay = startDay:endDay
     % Read from Fechas derrame and init proper number of particles. In a function
     spillData = spillData.splitByTimeStep(modelConfig, currDay);
   end
-  
-  if advectingParticles
-    for currHour = 0:modelConfig.timeStep:24-modelConfig.timeStep
-      currTime = toSerialDate(modelConfig.startDate.Year, currDay, currHour);
-      nextTime = toSerialDate(modelConfig.startDate.Year, currDay, currHour+modelConfig.timeStep);
-%       if advectingParticles   % creo que este if sobra
-        % Add the proper number of particles for this time step
-        Particles = initParticles(Particles, spillData, modelConfig, currDay, currHour);
-        
-        % Read winds and currents
-        VF = VF.readUV(currHour, currDay, modelConfig);
-        
-        % Advect particles
-        Particles = advectParticles(VF, modelConfig, Particles, nextTime);
-        
-        % Degrading particles
-        Particles = oilDegradation(Particles, modelConfig, spillData);
-%       end                     % creo que este if sobra
-      %  Visualize the current step
-      if modelConfig.visualize
-        %currentVis = currentVis.drawLiveParticles3D(Particles, modelConfig, currTime);
-        currentVis = currentVis.drawLiveParticles2D(Particles, modelConfig, currTime);
-      end
-    end
+    
+    if advectingParticles
+        for currHour = 0:modelConfig.timeStep:24-modelConfig.timeStep
+            currTime = toSerialDate(modelConfig.startDate.Year, currDay, currHour);
+            nextTime = toSerialDate(modelConfig.startDate.Year, currDay, currHour+modelConfig.timeStep);
+            %       if advectingParticles   % creo que este if sobra
+            % Add the proper number of particles for this time step
+            Particles = initParticles(Particles, spillData, modelConfig, currDay, currHour);
+            
+            % Read winds and currents
+            VF = VF.readUV(currHour, currDay, modelConfig);
+            
+            % Advect particles
+            if strcmp(modelConfig.model,'hycom')
+                Particles = advectParticles(VF, modelConfig, Particles, nextTime);
+            elseif strcmp(modelConfig.model,'adcirc')
+                Particles = advectParticlesADCIRC(VF, modelConfig, Particles, nextTime);
+            end
+            
+            % Degrading particles
+            Particles = oilDegradation(Particles, modelConfig, spillData);
+            %       end                     % creo que este if sobra
+            %  Visualize the current step
+            if modelConfig.visualize
+                if strcmp(modelConfig.vistype,'3D')
+                    currentVis = currentVis.drawLiveParticles3D(Particles, modelConfig, currTime);
+                elseif strcmp(modelConfig.vistype,'2D')
+                    currentVis = currentVis.drawLiveParticles2D(Particles, modelConfig, currTime);
+                elseif strcmp(modelConfig.vistype,'coastline')
+                    currentVis = currentVis.drawParticles2DCoastline(VF,Particles,modelConfig,currTime,currHour,currDay);
+                end
+            end
+        end
   end
   
   % Every 5 days we move away the non active particles.
